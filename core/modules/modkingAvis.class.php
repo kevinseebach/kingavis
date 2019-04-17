dfa4478fa838bf6fbf83f68123522450d3688531
abraham63 <kevincbar@gmail.com>
2019-04-17 16:24:28 +0200

    add Dolibarr module dependencies

--- core/modules/modkingAvis.class.php
+++ core/modules/modkingAvis.class.php
@@ -62,7 +62,7 @@ class modkingAvis extends DolibarrModules
 		$this->dirs = array();
 		$this->config_page_url = array("setup.php@kingavis");
 		$this->hidden = false;			// A condition to hide module
-		$this->depends = array();		// List of module class names as string that must be enabled if this module is enabled
+		$this->depends = array("Facture");		// List of module class names as string that must be enabled if this module is enabled
 		$this->requiredby = array();	// List of module ids to disable if this one is disabled
 		$this->conflictwith = array();	// List of module class names as string this module is in conflict with
 		$this->phpmin = array(5,3);					// Minimum version of PHP required by module
